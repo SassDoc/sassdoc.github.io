ffc1c15eb92ab881b2a539854ed019acf6d64168
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-09-28 22:42:44 +0200

    Added anchors to headers (#8)

---
+++ assets/js/anchors.js
@@ -0,0 +1,30 @@
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "#";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.appendChild(anchorForId(header.id));
+    }
+  }
+};
+
+document.onreadystatechange = function () {
+  if (this.readyState === "complete") {
+    var contentBlock = document.getElementsByClassName("main")[0];
+    if (!contentBlock) {
+      return;
+    }
+    for (var level = 2; level <= 6; level++) {
+      linkifyAnchors(level, contentBlock);
+    }
+  }
+};
